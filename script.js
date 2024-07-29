c7c0d7e10a8d874713e980c79705fe1f979b7fbb
KSHITIJ <135329525+GH0ST33333@users.noreply.github.com>
2024-07-29 22:20:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    document.getElementById('message').classList.remove('hidden');
+    setTimeout(function() {
+        window.location.href = "https://gh0st33333.github.io/animated-flower-bouquet/";
+    }, 5000); // Adjust delay as needed
+}
